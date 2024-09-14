c9d66683390b3b45a911efdef502d883c9ee8d4e
capzlock35 <capzlock35@gmail.com>
2024-09-15 02:14:46 +0800

    Login.0

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,73 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom' 
+import { FaUser } from "react-icons/fa";
+import { FaLock } from "react-icons/fa";
+import { MdAdminPanelSettings } from "react-icons/md";
+
+
 
 const Login = () => {
   return (
     <>
 
-
-        <div className='flex justify-center items-center min-h-screen hero bg'>
+            {/* 1st Div Website  */}   {/*2nd Div Container */} {/*3rd Div Login Container*/}
+        <div className='flex justify-center items-center min-h-screen hero bg-white'>
             <div className='flex w-full hero-content'>
+                <div className='card w-[600px] border border-red-600 shadow-2xl rounded-lg p-3'>
+                    <form onSubmit={""} className='card-body'>
+                        <h1 className='text-lg text-center font-bold text-black mb-4 leading-none'>LOGIN</h1>
+                        <div className='flex flex-col'>
 
+                                                    {/* Username */}
+                            <label className='label'>
+                                <span className='label-text font-semibold text-lg text-gray-500'>Username</span>
+                            </label>
+                            <div className='relative mb-2'>
+                            <input type="text"
+                            placeholder='Enter Your Username'
+                            className='input input-bordered w-full py-3 pl-10 pr-4 text-lg border-red-300 rounded-lg focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200' 
+                            value={""}
+                            onChange={""}
+                            />
+                            <FaUser className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500'/>
+                            </div>
+                            
+                                                    {/* Password */}
+                                <div className='relative'>
+                                    <div className='form-control mb-6'>
+                                        <label className='label mb-2'>
+                                            <span className='label-text font-semibold text-lg text-gray-500'>Password</span>
+                                        </label>
+                                        <input type="passowrd"
+                                        placeholder='Enter Your Password'
+                                        className='input input-bordered w-full py-3 pl-10 pr-4 text-lg
+                                        border-red-500 focus:border-blue-500 focus:ring-2 focus:ring-blue-200'
+                                        value={""}
+                                        onChange={""} />
+                                        <FaLock className='absolute left-3 top-1/2 transform translate-y-1/2 text-gray-500' />
+                                    </div>
+                                </div>
+
+                                
+                                                    {/* BTN */}
+                                                    <div className='form-control'>
+                                        <button className='btn btn-primary w-full py-2 text-lg font-semibold bg-blue-500 hover:bg-blue-700 text-white rounded-lg'> LOGIN
+                                        </button>
+                                    </div>
+                        </div>
+                    </form>
+                </div>
+
+                                                    {/* TEXT RIGHT SIDE */}
+                    <div className='text-center lg-text-lg w-full'>
+                        
+                        <h1 className=' flex items-center justify-center text-8xl font-extrabold text-black leading-none'> <MdAdminPanelSettings /> <span>ADMIN</span>
+                        </h1>
+                        <p className='py-6 text-2xl text-gray-500'>
+                            Login to access you account.
+                        </p>
+                    </div>
             </div>
-
         </div>
     </>
   )
