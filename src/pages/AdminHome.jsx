c57c709370c2ce3db132bb20b763f07e696aa802
capzlock35 <capzlock35@gmail.com>
2024-09-21 04:02:22 +0800

    ..

--- src/pages/AdminHome.jsx
+++ src/pages/AdminHome.jsx
@@ -2,16 +2,25 @@ import React from 'react'
 import Sidebar from '../components/Sidebar'
 import Search from '../components/Search'
 import Dashboard from '../components/Dashboard'
+import { Route, Routes } from 'react-router-dom'
+import DocumentStorage from '../components/DocumentStorage'
+
 
 const AdminHome = () => {
   return (
-    <div className="flex">
-    <Sidebar />
-    <div className="flex-col w-full">
-      <Search />
-      <Dashboard />
+
+      <div className="flex h-screen overflow-auto">
+      <Sidebar  />
+      <div className="flex-col w-full">
+        <Search />
+        <Routes>
+          <Route path='/' element={<Dashboard/>} />
+          <Route path='DocumentStorage' element={<DocumentStorage/>} />
+
+        </Routes>
+      </div>
     </div>
-  </div>
+
   )
 }
 
