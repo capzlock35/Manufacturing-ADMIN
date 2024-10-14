d23a562b712894dc5d97e90cbfc85464c1ad05dd
Kent Russel Olanda <olandakent10@gmail.com>
2024-10-14 08:32:20 +0800

    halaa

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -12,8 +12,49 @@ const getAllUser = async(req,res) => {
     catch (error){
         res.status(501).json({error: 'Unable to get users'})
     }const gettingUser = await User.find();
-
 } 
+
+const viewUser = async(req,res) => {
+    try{
+        const userId = req.params.id;
+        const user = await User.findById(userId);
+        if (!user){
+            return res.status(401).json({ error: 'Invalid credentials' })
+        }
+        res.status(200).json({ user });
+    } catch (error) {
+        res.status(500).json({ error: 'Error logging in' })
+    }
+}
+
+const updateUser = async(req,res) => {
+    try{
+        const userId = req.params.id;
+        const { firstname, lastname, birthday, gender, email, username, password } = req.body
+        const hashedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.findByIdAndUpdate( userId, {firstname, lastname, birthday, gender, email, username, password:hashedPassword}, { new: true} );
+        res.status(201).json({ message: 'User Updated successfully' })
+        if (!updateUser){
+            return res.status(401).json({ error: 'Invalid credentials' })
+        }
+        } catch (error) {
+            res.status(500).json({ error: 'Failed to Update' })
+        }
+    }
+
+const deleteUser = async (req, res) =>{
+    try{
+        const userId = req.params.id;
+        const user = await User.findByIdAndDelete(userId);
+        res.status(201).json({ message: 'User was Deleted successfully' })
+        if (!user){
+            return res.status(401).json({ error: 'Invalid credentials' })
+        }
+    } catch (error) {
+        res.status(500).json({ error: 'Error to delete' })
+    }
+}
 
 
     const createUser = async(req,res) => {
@@ -46,4 +87,4 @@ const getAllUser = async(req,res) => {
     }
 
 
-export {getAllUser,createUser,Login};
+export {getAllUser,updateUser,viewUser,deleteUser,createUser,Login};
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import {getAllUser,createUser,Login} from '../controller/userController.js'
+import {getAllUser,createUser,Login,updateUser,deleteUser, viewUser} from '../controller/userController.js'
 
 const  userRouter = express.Router();
 
 userRouter.get("/get",getAllUser);
 userRouter.post("/create",createUser);
 userRouter.post('/login', Login);
-
+userRouter.get("/view/:id", viewUser)
+userRouter.put("/update/:id", updateUser);
+userRouter.delete("/delete/:id", deleteUser);
 //GET Registered users
 
 //userRouter.put("/update/:id",updateUser);
