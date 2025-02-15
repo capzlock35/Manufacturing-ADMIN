f23ce2768dda056bc3f24a065da3a74a43010e7d
capzlock35 <capzlock35@gmail.com>
2025-02-15 21:35:42 +0800

    model

--- backend/model/financeModel.js
+++ backend/model/financeModel.js
@@ -1,6 +1,16 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+  image:{
+    public_id:{
+        type: String,
+        required: true
+    },
+    secure_url:{
+        type:String,
+        required: true
+    }
+},
 userName:{ type: String, required: true },
 password:{ type: String, required: true },
 email:{ type: String, required: true },
@@ -8,6 +18,6 @@ fullName:{ type: String, required: true },
 role:{type: String, required: true }
 }, {timestamp: true})
 
-const Finance = mongoose.model('Financeuser', userSchema);
+const Finance = mongoose.model('Finance', userSchema);
 
 export default Finance;
