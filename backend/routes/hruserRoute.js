340a97f7d93876c3869f38bd2ab3fdbac8f8cc5c
Kent Russel Olanda <olandakent10@gmail.com>
2025-02-16 22:01:07 +0800

    commit na please

--- backend/routes/hruserRoute.js
+++ backend/routes/hruserRoute.js
@@ -1,11 +1,10 @@
 import express from 'express';
-import {createUser, Login, getAllUser, deleteUser} from '../controller/hrController.js'
+import {createUser, getAllUser, deleteUser} from '../controller/hrController.js'
 
 const  hruserRouter = express.Router();
 
 hruserRouter.get("/get",getAllUser);
 hruserRouter.post("/create",createUser);
-hruserRouter.post('/login', Login);
 hruserRouter.delete('/delete/:id', deleteUser);
 
 export default hruserRouter;
--- backend/routes/logisticuserRoute.js
+++ backend/routes/logisticuserRoute.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import {createUser, Login, getAllUser, deleteUser, updateUser} from '../controller/logisticController.js'
+import {createUser, getAllUser, deleteUser, updateUser} from '../controller/logisticController.js'
 
 const  logisticuserRouter = express.Router();
 
 logisticuserRouter.get("/get",getAllUser);
 logisticuserRouter.post("/create",createUser);
 logisticuserRouter.post("/", createUser);
-logisticuserRouter.post('/login', Login);
 logisticuserRouter.delete('/:id', deleteUser);
 logisticuserRouter.put('/:id',updateUser );
 
