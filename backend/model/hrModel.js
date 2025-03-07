fe76690ed9390ed67505dd43d0f809c032d1b05b
Kent Russel Olanda <olandakent10@gmail.com>
2025-03-07 12:36:31 +0800

    hr

--- backend/model/hrModel.js
+++ backend/model/hrModel.js
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
+  employeeId: { type: Number, required: true },
   firstName: { type: String, required: true },
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
@@ -12,12 +13,10 @@ const userSchema = new mongoose.Schema({
   },
   Hr: {
     type: Number,
-    enum: [1, 2, 3, 4], 
-    required: true,
+    enum: [1, 2, 3, 4],
   },
   position: { 
-    type: String,
-    enum: ['CEO', 'Secretary', 'Production Head', 'Resellers Sales Head', 'Resellers'], 
+    type: String, 
     required: true,
   },
 });
